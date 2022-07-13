61a6e6bb8a9cf2b8a9d4ba5795a975cae0c1a566
Zener_085 <zener.085@mail.ru>
2022-07-13 11:20:13 +0300

    Final update

--- codes/data_structures/List/ListInterface.h
+++ codes/data_structures/List/ListInterface.h
@@ -32,13 +32,17 @@ class ListInterface {
     /// add new value to any place
     /// \param i index of the element the list has to store
     /// \param t value of the element the list has to store
-    /// \time_complexity best case - O(1), average case - O(n), worst case - O(n)
+    /// \time_complexity best case - O(1),
+    ///                  average case - O(n),
+    ///                  worst case - O(n)
     virtual void add(int i, T t) = 0;
 
     /// remove the element from the list
     /// \param i index of the element which has to be removed
     /// \return value of the removed element
-    /// \time_complexity best case - O(1), average case - O(n), worst case - O(n)
+    /// \time_complexity best case - O(1),
+    ///                  average case - O(n),
+    ///                  worst case - O(n)
     virtual T remove(int i) = 0;
 };
 
