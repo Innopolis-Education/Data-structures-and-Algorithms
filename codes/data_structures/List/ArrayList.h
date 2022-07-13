d36e64b4a725c62c25051ce699e711f13073b466
Zener_085 <zener.085@mail.ru>
2022-07-13 11:16:11 +0300

    Configure clang and cpplint

--- codes/data_structures/List/ArrayList.h
+++ codes/data_structures/List/ArrayList.h
@@ -7,62 +7,63 @@
 
 #include "ListInterface.h"
 
-template <typename T> class ArrayList : public ListInterface<T> {
-  int size_arr = 0;
-  T *array;
+template<typename T>
+class ArrayList : public ListInterface<T> {
+    int size_arr = 0;
+    T *array;
 
  public:
-  int CAPACITY = 16;
+    int CAPACITY = 16;
 
-  ArrayList() { this(CAPACITY); }
+    ArrayList() { this(CAPACITY); }
 
-  explicit ArrayList(int capacity) { array = new T[capacity]; }
+    explicit ArrayList(int capacity) { array = new T[capacity]; }
 
-  int size() { return size_arr; }
+    int size() { return size_arr; }
 
-  bool isEmpty() { return size_arr == 0; }
+    bool isEmpty() { return size_arr == 0; }
 
-  T get(int i) {
-    if (i >= size_arr) return (T)NULL;
+    T get(int i) {
+        if (i >= size_arr) return (T) NULL;
 
-    return array[i];
-  }
-
-  T set(int i, T t) {
-    if (i >= size_arr) return (T)NULL;
-
-    T removed = array[i];
-    array[i] = t;
-
-    return removed;
-  }
-
-  void add(int i, T t) {
-    size_arr++;
-
-    if (i >= size_arr) return;
-    T temp;
-
-    while (i < size_arr) {
-      temp = array[i];
-      array[i++] = t;
-      t = temp;
+        return array[i];
     }
-  }
 
-  T remove(int i) {
-    if (i >= size_arr) return (T)NULL;
+    T set(int i, T t) {
+        if (i >= size_arr) return (T) NULL;
 
-    T removed = array[i];
+        T removed = array[i];
+        array[i] = t;
 
-    while (i < size_arr - 1) {
-      array[i++] = array[i + 1];
+        return removed;
     }
-    array[i] = (T)NULL;
 
-    size_arr--;
-    return removed;
-  }
+    void add(int i, T t) {
+        size_arr++;
+
+        if (i >= size_arr) return;
+        T temp;
+
+        while (i < size_arr) {
+            temp = array[i];
+            array[i++] = t;
+            t = temp;
+        }
+    }
+
+    T remove(int i) {
+        if (i >= size_arr) return (T) NULL;
+
+        T removed = array[i];
+
+        while (i < size_arr - 1) {
+            array[i++] = array[i + 1];
+        }
+        array[i] = (T) NULL;
+
+        size_arr--;
+        return removed;
+    }
 };
 
 #endif  // CODES_DATA_STRUCTURES_LIST_ARRAYLIST_H_
